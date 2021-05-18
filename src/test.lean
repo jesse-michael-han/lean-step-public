d2563d74797c4277a9ac168a20c9840af345cb95
Stanislas Polu <spolu@openai.com>
2021-05-18 15:42:57 +0200

    nits

--- src/test.lean
+++ src/test.lean
@@ -7,21 +7,17 @@ import lean_step
 -- #check times_cont_diff_at_of_subsingleto
 
 #check continuous_linear_map.complete_space
-
 #check abs_max_sub_max_le_abs
 
 
-
-  theorem triangle_inequality {α} [_inst_1 : normed_group α]
-    (g h : α) : ∥g + h∥ ≤ ∥g∥ + ∥h∥ :=
-  begin
-    convert dist_triangle g 0 (-h); rw [dist_eq_norm],
-      { rw [sub_neg_eq_add] },
-      { rw [sub_zero] },
-      { rw [sub_neg_eq_add, zero_add] }
-  end
-
-
+theorem triangle_inequality {α} [_inst_1 : normed_group α]
+  (g h : α) : ∥g + h∥ ≤ ∥g∥ + ∥h∥ :=
+begin
+  convert dist_triangle g 0 (-h); rw [dist_eq_norm],
+    { rw [sub_neg_eq_add] },
+    { rw [sub_zero] },
+    { rw [sub_neg_eq_add, zero_add] }
+end
 
 run_cmd do {
   pf ← tactic.get_proof_from_env `norm_add_le >>= expr.unfold_macros,
