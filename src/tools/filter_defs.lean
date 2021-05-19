676c0ac2339ad64edcca1b532bf139056ab06841
Jesse Michael Han <hyrodi@gmail.com>
2021-05-19 12:21:33 -0400

    [src/tools/filter_defs] fix mysterious stack space error

--- src/tools/filter_defs.lean
+++ src/tools/filter_defs.lean
@@ -3,17 +3,12 @@ import all
 
 section main
 
-meta def main : io unit := do {
-  args ← io.cmdline_args,
-  names_file ← args.nth_except 0 "names_file",
-  dest ← args.nth_except 1 "dest",
+meta def main_aux (names_file : string) (dest : string) : io unit := do {
   nm_strs ← (io.mk_file_handle names_file io.mode.read >>= λ f,
     (string.split (λ c, c = '\n') <$> buffer.to_string <$> io.fs.read_to_end f)),
 
-  (nms : list (name × list name)) ← (nm_strs.filter $ λ nm_str, string.length nm_str > 0).mmap $ λ nm_str, do {
-    ((io.run_tactic' ∘ parse_decl_nm_and_open_ns) $ nm_str)
-  },
-
+  let nm_strs := nm_strs.filter (λ x : string, x.length > 0),
+  nms : list (name × list name) ← io.run_tactic' $ nm_strs.mmap parse_decl_nm_and_open_ns,
   dest_handle ← io.mk_file_handle dest io.mode.write,
  
   io.run_tactic' $ do {
@@ -32,6 +27,14 @@ meta def main : io unit := do {
       }
     }
   }
+}
+
+meta def main : io unit := do {
+  io.put_str_ln' "ENTERING",
+  args ← io.cmdline_args,
+  names_file ← args.nth_except 0 "names_file",
+  dest ← args.nth_except 1 "dest",
+  main_aux names_file dest
 }
 
 end main
